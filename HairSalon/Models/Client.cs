e4c32ef5c1a9185041da70cd28c1f154593b3ee8
Jeremy Josol <jeremyjosol@gmail.com>
2023-10-06 11:05:04 -0700

    Create Stylist + Client classes

---
+++ HairSalon/Models/Client.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+
+namespace HairSalon.Models
+{
+  public class Client
+  {
+    public int ClientId { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public int PhoneNumber { get; set; }
+    public DateTime? ScheduledAppointment { get; set; }
+    public int StylistId { get; set; }
+    public Stylist Stylist { get; set; }
+  }
+}
---
+++ HairSalon/Models/Stylist.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+
+namespace HairSalon.Models
+{
+  public class Stylist
+  {
+    public int StylistId { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public int PhoneNumber { get; set; }
+    public List<Client> Clients { get; set; }
+  }
+}
