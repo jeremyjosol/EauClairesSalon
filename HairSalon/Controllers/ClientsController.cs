06af332c37b3cb4de0f75085c0ee6c05a98848de
Jeremy Josol <jeremyjosol@gmail.com>
2023-10-06 12:14:48 -0700

    Add Index action + corresponding View

---
+++ HairSalon/Controllers/ClientsController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using HairSalon.Models;
+
+namespace HairSalon.Controllers
+{
+  public class ClientsController : Controller
+  {
+    private readonly HairSalonContext _db;
+    public ClientsController(HairSalonContext db)
+    {
+      _db = db;
+    }
+    public ActionResult Index()
+    {
+      List<Client> model = _db.Clients
+                                  .Include(client => client.Stylist)
+                                  .ToList();
+      return View(model);
+    }
+  }
+}
